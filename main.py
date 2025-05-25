4b3605c48c4810aac9c8947fc980264b0002e8a3
Aymeric <brouez.aymeric@gmail.com>
2025-05-25 18:57:03 +0200

    Added folder structure with UI
    main.py first draft calling UI

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+import webbrowser
+
+# === This starts the UI ===
+def launch_copilot():
+    print("Launching Copilot...")
+    webbrowser.open("UI\index.html")
+
+launch_copilot()
+
+
+# === This starts the Copilot ===
+def launch():
+    print("launchCopilot.py is running ...")
+    print("Checking for script path...")
+    
+    python_path = "C:\Python312\python.exe" # this makes sure Rhino uses Python3 instead of iron python 
+    script_path = "" # here, place the python scripts you want to run
+
+
+    if os.path.exists(python_path) and os.path.exists(script_path):
+        subprocess.Popen([python_path, script_path], creationflags=0)
+    else:
+        print("File not found")
+        print(" - Python path:", python_path)
+        print(" - Script path:", script_path)
+
+launch()
