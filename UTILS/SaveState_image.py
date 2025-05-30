0dd2a401e1b71370c3157bcfe3e26ec105b6c903
Aymeric <brouez.aymeric@gmail.com>
2025-05-30 11:19:10 +0200

    SaveState_image WIP
    To be included inside Andres' ML.py
    It will reuse the {version} to be able to run

---
+++ UTILS/SaveState_image.py
@@ -0,0 +1,43 @@
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+import Rhino
+
+import System
+import os
+import datetime
+
+# call this viewport capture from ML with:
+# from UTILS.SaveState_image import capture_viewport
+
+
+def capture_viewport_to_knowledge(version_name="unnamed"): # takes the input "version name" from the calling file
+    import rhinoscriptsyntax as rs
+    import scriptcontext as sc
+    import Rhino
+    import System
+
+    base_dir = os.path.dirname(__file__)
+    knowledge_dir = os.path.abspath(os.path.join(base_dir, "..", "KNOWLEDGE")) # check sub-folder thing
+    os.makedirs(knowledge_dir, exist_ok=True)
+
+    #file name to be similar to the iteration.json
+    filename = f"{version_name}.png"
+    filepath = os.path.join(knowledge_dir, filename)
+
+    view = sc.doc.Views.ActiveView
+    if not view:
+        print("No active view found.")
+        return None
+
+    #maybe we will have constrainted width & height? Square? Or not
+    width = view.ActiveViewport.Size.Width
+    height = view.ActiveViewport.Size.Height
+    bitmap = view.CaptureToBitmap(Rhino.Display.ViewCaptureSettings(view, width, height, True))
+
+    if bitmap:
+        bitmap.Save(filepath, System.Drawing.Imaging.ImageFormat.Png)
+        print(f"Screenshot saved to: {filepath}")
+        return filepath
+    else:
+        print("Failed to capture viewport.")
+        return None
